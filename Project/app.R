a5ef5e96a1557c94cc0474cc77a4d3579b0db681
Gaurav Kandoi <kandoigaurav@gmail.com>
2017-05-03 10:40:04 -0500

    Update

--- Project/app.R
+++ Project/app.R
@@ -1,9 +1,14 @@
 library(shiny)
 library(ggplot2)
 library(plotly)
-library(crosstalk)
 library(readr)
-library(ggvis)
+library(lubridate)
+# Load datasets
+
+HSA <- unique(read.table("./data/goa_human.gaf",skip = 34, header = F, sep = "\t", strip.white = F, stringsAsFactors = T, skipNul = T, quote = "",comment.char = "")[,c(3,5,7,9,14)])
+MMU <- unique(read.table("./data/gene_association.mgi",skip = 47, header = F, sep = "\t", strip.white = F, stringsAsFactors = T, skipNul = T, quote = "",comment.char = "")[,c(3,5,7,9,14)])
+HSA$V14 <- as.character(ymd(HSA$V14))
+MMU$V14 <- as.character(ymd(MMU$V14))
 
 # Define UI for application
 ui <- fluidPage(    
@@ -12,25 +17,26 @@ ui <- fluidPage(
 # Sidebar with a radio button to select the Organism
   sidebarPanel(
     radioButtons("radio", label = h3("Choose organism"),
-                 choices = list("SGN" = 1, "HS-rna" = 2), 
-                 selected = NULL),
-    
-    hr(),
-    fluidRow(column(10, verbatimTextOutput("value"))),
+                 choices = list("Human" = 1, "Mouse" = 2), 
+                 selected = 1),
     conditionalPanel(
-      condition = "input.conditionedPanels == 'Bar Plot of Genes'",uiOutput("data")),
+      condition = "input.conditionedPanels == 'Table'", numericInput(inputId = "n","# of Annotations", value = 10)),
     conditionalPanel(
-      condition = "input.conditionedPanels == 'Bar Plot of Go terms'",uiOutput("data2"))
+      condition = "input.conditionedPanels == 'Bar Plot of Genes'", uiOutput("data")),
+    conditionalPanel(
+      condition = "input.conditionedPanels == 'Bar Plot of Go terms'",uiOutput("data2")),
+    conditionalPanel(
+      condition = "input.conditionedPanels == 'Growth'", uiOutput("data3"))
   ),
   
 # Define the tab panels
   mainPanel(
     tabsetPanel(
-      tabPanel("Summary", verbatimTextOutput("summary")),
       tabPanel("Table", tableOutput("view")),
       tabPanel("Plot", plotlyOutput("aspect")),
       tabPanel("Bar Plot of Genes",plotlyOutput("bars")),
       tabPanel("Bar Plot of Go terms",plotlyOutput("bars2")),
+      tabPanel("Growth", plotlyOutput("bars3")),
       id = "conditionedPanels"
     )
   )
@@ -40,59 +46,63 @@ ui <- fluidPage(
 # Download the current annotations from the GO server.
 server <- function(input, output) {
   datasetInput <- reactive({
-    txt <- readLines(gzcon(url("http://geneontology.org/gene-associations/gene_association.sgn.gz")))
-    SGN_data <- read.table(textConnection(txt),skip = 24, header = F, sep = "\t", strip.white = F, stringsAsFactors = T, skipNul = T, quote = "",comment.char = "")
-    txt <- readLines(gzcon(url("http://geneontology.org/gene-associations/goa_human_rna.gaf.gz")))
-    HS_rna_data <- read.table(textConnection(txt),skip = 30, header = F, sep = "\t", strip.white = F, stringsAsFactors = T, skipNul = T, quote = "",comment.char = "")
-    
     if (input$radio == 1) {
-      SGN_data
+      HSA
     }  else {
-      HS_rna_data
+      MMU
     }
   })
 
-# Display the summary of data
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
-# Show the first "10" observations
+# Show the first "10" observations and give user an option to view more than 10.
   output$view <- renderTable({
-    head(datasetInput())
+    head(datasetInput(), n = input$n)
   })
 # Plot the number of annotations for every evidence code, colored by the aspect of annotations.
   output$aspect <- renderPlotly({
     gg <-  ggplot(data = datasetInput(), aes(x = V7, fill = V9)) +
-      geom_bar(stat = "count")
+      geom_bar(position = "dodge")
     ggplotly(gg)
   })
+
 # Extract the list of genes and provide to the user a drop-down menu.
   output$data <- renderUI({
     data <- datasetInput()
-    selectInput("genes",label="gene",choices=sort(unique(data$V3)),selected=data$V3[1])
+    selectInput("genes",label="gene",choices=sort(unique(data$V3)), size = 5, selectize=F,selected=data$V3[1])
   })
   
 # Plot the number of annotations for every gene based on the aspect of annotations and colored by the evidence code.
   output$bars <- renderPlotly({
     gg <-  datasetInput() %>% filter(V3==input$genes) %>% ggplot(aes(x=V9, fill = V7))+
-      geom_bar(stat = "count")
+      geom_bar(position = "dodge")
     ggplotly(gg)
   })
 
 # Extract the list of GO terms and provide to the user a drop-down menu.
   output$data2 <- renderUI({
-    data <- datasetInput()
-    selectInput("goterms",label="go terms",choices=sort(unique(data$V5)),selected=data$V5[1])
+    data2 <- datasetInput()
+    selectInput("goterms",label="go terms",choices=sort(unique(data2$V5)), size = 5, selectize=F,selected=data2$V5[1])
   })
   
 # Plot the number of annotations for every GO term based on the aspect of annotations and colored by the evidence code.  
   output$bars2 <- renderPlotly({
-    gg <-  datasetInput() %>% filter(V5==input$goterms) %>% ggplot(aes(x=V9, fill = V7))+
-      geom_bar(stat = "count")
+    gg <-  datasetInput() %>% filter(V5==input$goterms) %>% ggplot(aes(x=V9, fill = V7)) +
+      geom_bar(position = "dodge")
     ggplotly(gg)
   })
+
+# Extract the list of genes and provide to the user a drop-down menu.
+  output$data3 <- renderUI({
+    data3 <- datasetInput()
+    dateInput("DatePlot", "Date Plot", min=min(data3$V14), max=max(data3$V14), value=median(data3$V14))
+  })
+  
+# Plot the number of annotations for every gene based on the aspect of annotations and colored by the evidence code.
+  output$bars3 <- renderPlotly({
+    gg <-  datasetInput() %>% filter(V14==input$DatePlot) %>% ggplot(aes(x=V9, fill = V7)) +
+      geom_bar(position = "dodge")
+    ggplotly(gg)
+  })
+  
   
 }
 
